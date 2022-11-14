2bc10fa08f162c0133768cfa1b0578ee1c125036
SAULO MORALES OZÓRIO <saulo.m.ozorio@gmail.com>
2022-11-14 13:46:23 -0300

    Fix: Fixed Types of Input and Form component. Added React-Icons, Yup and Hook Form

--- src/components/common/container.tsx
+++ src/components/common/container.tsx
@@ -1,7 +1,9 @@
-interface ContainerProp {
+import React from "react";
+
+interface IContainerProp{
   children: React.ReactNode;
 }
 
-export const Container = ({ children }: ContainerProp): JSX.Element => {
+export const Container = ({ children }: IContainerProp): JSX.Element => {
   return <div className="p-5 max-w-3xl mx-auto">{children}</div>;
 };
